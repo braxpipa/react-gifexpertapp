e85554260ff41d1fc2a524b3bf04b1bd7abcef9a
BrandonPineda <braxpipa@gmail.com>
2020-08-29 21:47:22 -0500

    docs agregado

---
+++ docs/precache-manifest.3fe67576c04839adba903eff47ed4b7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0db132e1de73b5cd153ed7fa3d24bc53",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3740a875b29b2df289d",
+    "url": "/static/css/main.d0ae0ae8.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3740a875b29b2df289d",
+    "url": "/static/js/main.6e0db1e1.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
